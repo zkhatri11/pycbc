330c7af1795fd540b2c2623a0a87429e605f79ca
Zaryab Ahmed <zaryaba@ux.uis.no>
2023-05-06 21:27:13 +0200

    damping times fixed

--- pycbc/conversions.py
+++ pycbc/conversions.py
@@ -984,15 +984,16 @@ def get_JP_lm_f0tau(mass,spin,epsilon,l,m,n=0, which='both'):
     gettau = which == 'both' or which == 'tau'
     out = []
     if getf0:
-        f0s = pykerr.qnmfreq(mass, spin, l, m, n) + ((l*epsilon*((1/(81.*numpy.sqrt(3))+((10.*spin)/(729.))+((47.*spin**2)/(1458.*numpy.sqrt(3))))))/(2.*numpy.pi*mass))/lal.MTSUN_SI
+        f0s = pykerr.qnmfreq(mass, spin, l, m, n) + ((l*epsilon*((1/(81.*numpy.sqrt(3))+((10.*spin)/(729.))+((47.*spin**2)/(1458.*numpy.sqrt(3))))))/(2.*numpy.pi*mass*lal.MTSUN_SI))
         out.append(formatreturn(f0s, input_is_array))
     if gettau:
-        if epsilon==0.:
-            taus=pykerr.qnmtau(mass, spin, l, m, n)
-            out.append(formatreturn(taus, input_is_array))
-        else:
-            taus = pykerr.qnmtau(mass, spin, l, m, n) - ((1/(2.*numpy.pi*mass)*(2*n + 1)*epsilon*((spin)/(486.) + (16.*spin**2)/(2187.*numpy.sqrt(3))))/(lal.MTSUN_SI))**(-1)
-            out.append(formatreturn(taus, input_is_array))
+        #if epsilon == 0. or spin == 0:
+        #    taus=pykerr.qnmtau(mass, spin, l, m, n)
+        #    out.append(formatreturn(taus, input_is_array))
+        #else:
+            #taus = pykerr.qnmtau(mass, spin, l, m, n) - 2.*numpy.pi*mass*lal.MTSUN_SI*(1/epsilon)*(2*n + 1)*(1/((spin)/(486.) + spin**2*(16.)/(2187.*numpy.sqrt(3))))
+        taus = 1 / ((1 /pykerr.qnmtau(mass, spin, l, m, n)) - ((epsilon*((spin)/(486.) + spin**2*(16.)/(2187.*numpy. sqrt(3))))/(mass*lal.MTSUN_SI)))
+        out.append(formatreturn(taus, input_is_array))
     if not (getf0 and gettau):
         out = out[0]
     return out
